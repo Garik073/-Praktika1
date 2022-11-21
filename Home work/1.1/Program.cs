be1a0544c2653eecbabd0f57ad00a378bad9efe4
Igor <gariklukin073@gmail.com>
2022-11-22 00:16:23 +0400

    One work 1.1

--- Home work/1.1/Program.cs
+++ Home work/1.1/Program.cs
@@ -7,10 +7,12 @@ int b = int.Parse(Console.ReadLine());
 if (a > b)
 {
     Console.WriteLine("Первое число больше"); Console.WriteLine(a);
+    Console.WriteLine("Второе число меньше");Console.WriteLine(b);
 }
 else
 {
     Console.WriteLine("Второе число больше");Console.WriteLine(b);
+    Console.WriteLine("Первое число меньше");Console.WriteLine(b);
 }
 
 if (a == b)
