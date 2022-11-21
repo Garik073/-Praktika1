c33930fc15968943ba488f493b53515782430bf6
Igor <gariklukin073@gmail.com>
2022-11-22 00:06:44 +0400

    Work One

---
+++ Home work/1.1/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.WriteLine("Введите первое число");
+int a = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Введите второе число");
+int b = int.Parse(Console.ReadLine());
+
+if (a > b)
+{
+    Console.WriteLine("Первое число больше"); Console.WriteLine(a);
+}
+else
+{
+    Console.WriteLine("Второе число больше");Console.WriteLine(b);
+}
+
+if (a == b)
+{
+    Console.WriteLine("Они равны");
+}
