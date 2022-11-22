0408903afd423cfed8ac26f5c530295991cba83c
Igor <gariklukin073@gmail.com>
2022-11-22 17:23:32 +0400

    For work

---
+++ Home work/1.4/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Введите число");
+int a = int.Parse(Console.ReadLine());
+int b = 0;
+
+while (b <= a)
+{
+    Console.WriteLine(b);
+    //Console.Write($"{b*2} ");
+    b =b +2;
+}
